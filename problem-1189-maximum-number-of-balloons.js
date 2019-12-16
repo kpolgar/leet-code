c134925f44124693d38fa50e319db335ee2d5f93
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2019-12-16 05:27:23 -0500

    Create problem-1189-maximum-number-of-balloons.js

---
+++ problem-1189-maximum-number-of-balloons.js
@@ -0,0 +1,59 @@
+/*1189. Maximum Number of Balloons
+
+Given a string text, you want to use the characters of text to form as many
+instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of
+instances that can be formed.
+
+Example 1:
+
+Input: text = "nlaebolko"
+Output: 1
+Example 2:
+
+Input: text = "loonbalxballpoon"
+Output: 2
+Example 3:
+
+Input: text = "leetcode"
+Output: 0
+ 
+Constraints:
+
+1 <= text.length <= 10^4
+text consists of lower case English letters only.
+*/
+
+/*STATS
+Runtime: 64 ms, faster than 51.93% of JavaScript online submissions for Maximum Number of Balloons.
+Memory Usage: 35.1 MB, less than 100.00% of JavaScript online submissions for Maximum Number of Balloons.
+*/
+
+var maxNumberOfBalloons = function(text) {
+    let output = 0;
+    let map = {};
+    
+    for (let i = 0; i < text.length; i++) {
+        if(!map[text[i]]) {
+            map[text[i]] = 1;
+        } else {
+            map[text[i]] = map[text[i]] + 1
+        }
+    }
+    
+    
+    while(true) {
+        if (map['b'] >= 1 && map['a'] >= 1 && map['l'] >= 2 && map['o'] >= 2 && map['n'] >= 1 ) {
+            map['b'] = map['b'] - 1;
+            map['a'] = map['a'] - 1;
+            map['l'] = map['l'] - 2;
+            map['o'] = map['o'] - 2;
+            map['n'] = map['n'] - 1;
+            output = output + 1
+            
+        } else {
+            break;
+        } 
+    } return output;
+};
