17b01680b90588a5a218401aebc1c9d12d019483
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2020-01-31 05:26:44 -0500

    Create problem-0504-base-7.js

---
+++ problem-0504-base-7.js
@@ -0,0 +1,18 @@
+/*504. Base 7
+
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+Example 2:
+Input: -7
+Output: "-10"
+Note: The input will be in range of [-1e7, 1e7].*/
+
+/*Runtime: 44 ms, faster than 99.25% of JavaScript online submissions for Base 7.
+Memory Usage: 33.6 MB, less than 100.00% of JavaScript online submissions for Base 7.*/
+
+var convertToBase7 = function(num) {
+    return num.toString(7)       
+};
