6ac1b025ec5ad61bcbccabc8d80636c0c24444e3
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2020-07-23 06:57:40 -0400

    Create problem-1518-water-bottles.js

---
+++ problem-1518-water-bottles.js
@@ -0,0 +1,49 @@
+/*1518. Water Bottles
+
+Given numBottles full water bottles, you can exchange numExchange empty water bottles for one full water bottle.
+
+The operation of drinking a full water bottle turns it into an empty bottle.
+
+Return the maximum number of water bottles you can drink.
+
+Example 1:
+
+Input: numBottles = 9, numExchange = 3
+Output: 13
+Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+Number of water bottles you can drink: 9 + 3 + 1 = 13.
+Example 2:
+
+Input: numBottles = 15, numExchange = 4
+Output: 19
+Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+Number of water bottles you can drink: 15 + 3 + 1 = 19.
+Example 3:
+
+Input: numBottles = 5, numExchange = 5
+Output: 6
+Example 4:
+
+Input: numBottles = 2, numExchange = 3
+Output: 2
+ 
+Constraints:
+
+1 <= numBottles <= 100
+2 <= numExchange <= 100*/
+
+/*STATS Runtime: 68 ms, faster than 96.92% of JavaScript online submissions for Water Bottles.
+Memory Usage: 36.4 MB, less than 100.00% of JavaScript online submissions for Water Bottles.*/
+
+var numWaterBottles = function(numBottles, numExchange) {
+    let bottlesDrunk = numBottles;
+    let remainder = numBottles;
+    
+    while(remainder >= numExchange) {
+        let newBottles = Math.floor(remainder / numExchange);
+        bottlesDrunk += newBottles;
+        remainder = (remainder % numExchange) + newBottles;
+    }
+    
+    return bottlesDrunk;
+};
