33b88626002b3ba2aee473a76bdacb10c2ccb37e
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2020-11-17 07:25:12 -0500

    Create problem-0235-lowest-common-ancestor-of-a-binary-search-tree.js

---
+++ problem-0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,69 @@
+/*235. Lowest Common Ancestor of a Binary Search Tree
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+*/
+
+/*STATS Runtime: 96 ms, faster than 85.78% of JavaScript online submissions for Lowest Common Ancestor of a Binary Search Tree.
+Memory Usage: 48.5 MB, less than 73.00% of JavaScript online submissions for Lowest Common Ancestor of a Binary Search Tree.*/
+
+var lowestCommonAncestor = function(root, p, q) {
+    let pMap = {}
+    pMap[root.val] = true;
+    let node = root
+
+    while (node.val !== p.val) {
+        if (p.val < node.val) {
+            node = node.left
+        }
+        
+        else if (p.val > node.val) {
+            node = node.right
+        } 
+        pMap[node.val] = true;
+    }
+    node = root
+    
+    while (node.val !== q.val) {
+        if (q.val < node.val && pMap[node.left.val]) {
+                node = node.left
+        }
+        
+        else if (q.val > node.val && pMap[node.right.val]) {
+                node = node.right
+            }
+        else {
+            break
+        }
+    }
+    return node;
+};
