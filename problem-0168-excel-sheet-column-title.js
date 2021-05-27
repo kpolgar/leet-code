89d1427b2e71d1e807898ab8a2ee7fc22c95777f
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2021-05-27 07:04:02 -0400

    Create problem-0168-excel-sheet-column-title.js

---
+++ problem-0168-excel-sheet-column-title.js
@@ -0,0 +1,57 @@
+/*168. Excel Sheet Column Title
+Easy
+
+1720
+
+312
+
+Add to List
+
+Share
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+Example 4:
+
+Input: columnNumber = 2147483647
+Output: "FXSHRXW"
+ 
+
+Constraints:
+
+1 <= columnNumber <= 231 - 1*/
+
+/*STATS Runtime: 76 ms, faster than 67.03% of JavaScript online submissions for Excel Sheet Column Title.
+Memory Usage: 38.7 MB, less than 10.62% of JavaScript online submissions for Excel Sheet Column Title.*/
+
+var convertToTitle = function(n) {
+    const digits = [];
+    while (n > 0) {
+      digits.unshift((n - 1) % 26);
+      n = Math.trunc((n - 1) / 26);
+    }
+    return String.fromCharCode(...digits.map(d => d + 65));
+};
