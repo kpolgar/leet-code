2005ed362c3883b28b9bef0ee571731fc15421f8
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2021-05-27 07:04:29 -0400

    Update problem-0168-excel-sheet-column-title.js

--- problem-0168-excel-sheet-column-title.js
+++ problem-0168-excel-sheet-column-title.js
@@ -1,13 +1,5 @@
 /*168. Excel Sheet Column Title
-Easy
 
-1720
-
-312
-
-Add to List
-
-Share
 Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 
 For example:
